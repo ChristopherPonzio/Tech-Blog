c57a757caaf029c2525e5c741b23e21b2cbc2a34
Christopher Ponzio <chrisponz11@yahoo.com>
2022-04-03 12:16:01 -0400

    routes added

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
 
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes.js');
+const commentRoutes = require('./commentRoutes.js');
 
 router.use('/users', userRoutes);
-
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,54 +1,46 @@
 const router = require('express').Router();
-const { User, Post } = require('../models');
+const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
-  try {
-    // Get all projects and JOIN with user data
-    const postData = await Post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+// get all posts for homepage
+router.get("/", (req, res) => {
+  Post.findAll({
+      include: [User],
+  })
+      .then((dbPostData) => {
+          const posts = dbPostData.map((post) => post.get({ plain: true }));
 
-    // Serialize data so the template can read it
-    const post = postData.map((post) => post.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', {  
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+          res.render("all-posts", { posts });
+      })
+      .catch((err) => {
+          res.status(500).json(err);
+      });
 });
+    
 
 // get single post
 router.get("/post/:id", (req, res) => {
-    Post.findByPk(req.params.id, {
-        include: [
-            User,
-            {
-                model: Comment,
-                include: [User],
-            },
-        ],
-    })
-        .then((dbPostData) => {
-            if (dbPostData) {
-                const post = dbPostData.get({ plain: true });
+  Post.findByPk(req.params.id, {
+      include: [
+          User,
+          {
+              model: Comment,
+              include: [User],
+          },
+      ],
+  })
+      .then((dbPostData) => {
+          if (dbPostData) {
+              const post = dbPostData.get({ plain: true });
 
-                res.render("single-post", { post });
-            } else {
-                res.status(404).end();
-            }
-        })
-        .catch((err) => {
-            res.status(500).json(err);
-        });
+              res.render("single-post", { post });
+          } else {
+              res.status(404).end();
+          }
+      })
+      .catch((err) => {
+          res.status(500).json(err);
+      });
 });
 
 router.get('/login', (req, res) => {
