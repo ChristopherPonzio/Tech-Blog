88efd3da53827076e8c684c46472a8d39b202f74
Christopher Ponzio <chrisponz11@yahoo.com>
2022-04-09 09:14:07 -0400

    added dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -15,7 +15,7 @@ const newFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/dashboard');
     } else {
       alert('Failed to create post');
     }
@@ -31,7 +31,7 @@ const delButtonHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/dashboard');
     } else {
       alert('Failed to delete post');
     }
