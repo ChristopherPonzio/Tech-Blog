851a69a73db38de5e8c94e1edb159fc7c4be024d
Christopher Ponzio <chrisponz11@yahoo.com>
2022-04-10 10:33:31 -0400

    My second commit

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
@@ -32,7 +32,6 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
